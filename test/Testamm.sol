b8a85c5e2a4c7d1376cf3f0a250330f709e59d5b
Rishi Talreja <rishi_t@me.iitr.ac.in>
2025-03-18 06:22:36 +0000

    feat: add initial implementation of basic AMM contract

--- test/Testamm.sol
+++ test/Testamm.sol
@@ -6,7 +6,8 @@ import {Token1} from "../src/Token.sol";
 import {Token2} from "../src/Token.sol";
 contract Testamm is Test{
     Amm amm;
- uint256  intialsupply1=100000 ether;
+  uint256  intialsupply1=100000 ether;
+ uint256 intialsupply2 =100000 ether;
  function setUp() public{
     amm =new Amm();  
  }
@@ -56,4 +57,61 @@ function testvalue2()external{
       uint256 expected= 1e23/getsupplyvalue;
     assertEq(amm.valueoftoken2(tokenu),expected);
 }
+function testcheck() public {
+    uint256 supply1 = amm.gettokensupply(amm.gettoken1());
+    uint256 supply2 = amm.gettokensupply(amm.gettoken2());
+    uint256 actual =(intialsupply1)*(intialsupply2);
+    uint256 expected =(supply1)*(supply2);
+    assertEq(expected,actual);
+ }
+ function testcheck1()public{
+    address token1 = amm.gettoken1();
+    address token2 =amm.gettoken2();
+    uint256 amount = 5 ether;
+    uint256 intialsupply1 = amm.gettokensupply(token1);
+    uint256 intialsupply2 = amm.gettokensupply(token2);
+    amm.main(token1 , amount);
+ uint256 finalsupply1 = amm.gettokensupply(token1);
+ uint256 finalsupply2 = amm.gettokensupply(token2);
+ uint256 intialproduct = (intialsupply1)*(intialsupply2);
+uint256 finalproduct = (finalsupply1)*(finalsupply2);
+ }
+  function testcheck2()public{
+    address token1 = amm.gettoken1();
+    address token2 =amm.gettoken2();
+    uint256 amount = 5 ether;
+    uint256 intialsupply1 = amm.gettokensupply(token1);
+    uint256 intialsupply2 = amm.gettokensupply(token2);
+    amm.main(token2 , amount);
+ uint256 finalsupply1 = amm.gettokensupply(token1);
+ uint256 finalsupply2 = amm.gettokensupply(token2);
+ uint256 intialproduct = (intialsupply1)*(intialsupply2);
+uint256 finalproduct = (finalsupply1)*(finalsupply2);
+ }
+ function testcheck3()public{
+      address token1 = amm.gettoken1();
+    address token2 =amm.gettoken2();
+     uint256 intialsupply1 = amm.gettokensupply(token1);
+     uint256 intialsupply2 = amm.gettokensupply(token2);
+    uint256 amount1 = 5 ether;
+    uint256 amount2 =5 ether;
+    uint256 expectedfinalsupply1 = amm.gettokensupply(token1) + amount1;
+    uint256 expectedfinalsupply2 = amm.gettokensupply(token2) + amount2;
+    amm.deposit(amount1,amount2,token2,token1);
+  uint256 actualfinalsupply1 = amm.gettokensupply(token1);
+ uint256 actualfinalsupply2 = amm.gettokensupply(token2);
+ assertEq(expectedfinalsupply1,actualfinalsupply1);
+assertEq(expectedfinalsupply2,actualfinalsupply2);
+ }
+ function testcheck4()public{
+      address token1 = amm.gettoken1();
+    address token2 =amm.gettoken2();
+    uint256 amount = 5 ether;
+    amm.main(token2 , amount);
+    uint256 amount1 = 5 ether;
+    uint256 amount2 =5 ether;
+    vm.expectRevert();
+    amm.deposit(amount1,amount2,token2,token1);
+ }
+
 }
